2aa1922a40744ab8325c83cbc52410d232b89566
Kate Lien <c1988618@gmail.com>
2022-05-03 00:13:28 +0800

    加入專案檔案。

---
+++ 10_LINQ_HW_0502/Frm作業_1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _10_LINQ_HW_0502
+{
+    public partial class Frm作業_1 : Form
+    {
+        public Frm作業_1()
+        {
+            InitializeComponent();
+            LoadYearToCombobox();
+            this.ordersTableAdapter1.Fill(this.nwDataSet1.Orders);
+            this.bindingSource1.DataSource = this.nwDataSet1.Orders;
+            this.order_DetailsTableAdapter1.Fill(this.nwDataSet1.Order_Details);
+        }
+
+
+        private void LoadYearToCombobox()
+        {
+            DataClasses1DataContext dc = new DataClasses1DataContext();
+            var r = (from x in dc.Orders
+                     select x.OrderDate.Value.Year).Distinct().ToList();
+
+            comboBox1.ValueMember = "OrderDate";
+            comboBox1.DataSource = r;
+            comboBox1.SelectedIndex = 0;
+
+
+
+        }
+
+        private void bindingSource1_CurrentChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            this.dataGridView1.DataSource = this.nwDataSet1.Orders;
+            this.dataGridView2.DataSource = this.nwDataSet1.Order_Details;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var q = from o in nwDataSet1.Orders
+                    where o.OrderDate.Year == Convert.ToInt32(comboBox1.Text)
+                    select o;
+            this.dataGridView1.DataSource = q.ToList();
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+            System.IO.DirectoryInfo dir = new System.IO.DirectoryInfo(@"c:\windows");
+
+            System.IO.FileInfo[] files = dir.GetFiles();
+
+            this.dataGridView1.DataSource = files;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            System.IO.DirectoryInfo dir = new System.IO.DirectoryInfo(@"c:\windows");
+
+            System.IO.FileInfo[] files = dir.GetFiles();
+
+            var q = from k in files
+                    where k.CreationTime.Year == 2017
+                    select k;
+
+            this.dataGridView1.DataSource = q.ToList();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            System.IO.DirectoryInfo dir = new System.IO.DirectoryInfo(@"c:\windows");
+
+            System.IO.FileInfo[] files = dir.GetFiles();
+
+            var q = from n in files
+                    where n.Length > 10000
+                    select n;
+
+            this.dataGridView1.DataSource = q.ToList();
+        }
+    }
+}
---
+++ 10_LINQ_HW_0502/Frm作業_2.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _10_LINQ_HW_0502
+{
+    public partial class Frm作業_2 : Form
+    {
+        public Frm作業_2()
+        {
+            InitializeComponent();
+            LoadYearToCombobox();
+            this.productPhotoTableAdapter1.Fill(this.awDataSet1.ProductPhoto);
+
+        }
+
+        private void LoadYearToCombobox()
+        {
+            awDataClassesDataContext dr = new awDataClassesDataContext();
+
+            var q = (from x in dr.ProductPhoto
+                     select x.ModifiedDate.Year).Distinct().ToList();
+
+            this.comboBox3.SelectedValue = "ModifiedDate";
+            this.comboBox3.DataSource = q;
+
+
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            this.dataGridView1.DataSource = this.awDataSet1.ProductPhoto;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            var q = from x in awDataSet1.ProductPhoto
+                    where x.ModifiedDate.Year == Convert.ToInt32(comboBox3.Text)
+                    select x;
+            this.dataGridView1.DataSource = q.ToList();
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            //string 第一季 = "<=3";
+            //string 第二季 = ">3&&<=6";
+            //string 第三季 = ">6&&<=9";
+            //string 第四季 = ">9&&<=12";
+            //int 筆數;
+            //this.dataGridView1.DataSource = q.ToList();
+
+            int n = this.comboBox2.SelectedIndex;
+
+            if (n == 0)
+            {
+                var q = from x in awDataSet1.ProductPhoto
+                        where x.ModifiedDate.Month <= 3
+                        select x;
+                this.dataGridView1.DataSource = q.ToList();
+                lblMaster.Text = "Master 共" + q.ToList().Count + "筆";
+            }
+
+            if (n == 1)
+            {
+                var p = from x in awDataSet1.ProductPhoto
+                        where x.ModifiedDate.Month > 3 && x.ModifiedDate.Month <= 6
+                        select x;
+                this.dataGridView1.DataSource = p.ToList();
+                lblMaster.Text = "Master 共" + p.ToList().Count + "筆";
+            }
+
+            if (n == 2)
+            {
+                var r = from x in awDataSet1.ProductPhoto
+                        where x.ModifiedDate.Month > 6 && x.ModifiedDate.Month <= 9
+                        select x;
+                this.dataGridView1.DataSource = r.ToList();
+                lblMaster.Text = "Master 共" + r.ToList().Count + "筆";
+            }
+
+            if (n == 3)
+            {
+                var w = from x in awDataSet1.ProductPhoto
+                        where x.ModifiedDate.Month > 9 && x.ModifiedDate.Month <= 12
+                        select x;
+                this.dataGridView1.DataSource = w.ToList();
+                lblMaster.Text = "Master 共" + w.ToList().Count + "筆";
+
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            var q = from x in awDataSet1.ProductPhoto
+                    where dateTimePicker2.Value >= x.ModifiedDate && x.ModifiedDate >= dateTimePicker1.Value
+                    select x;
+            this.dataGridView1.DataSource = q.ToList();
+        }
+    }
+}
